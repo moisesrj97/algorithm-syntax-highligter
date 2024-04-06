89b65ee459fd81f808f425787f363d031d7eda69
Moises Rodriguez <moisesrj97@gmail.com>
2024-04-06 12:39:31 +0200

    feat: add basic syntax

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,52 @@ import './App.css';
 import { useState } from 'react';
 
 Prism.languages.your_language = {
-  'const-def': {
-    pattern: /(?:end )?const/,
+  'type-def-block': {
+    pattern: /\btype\b[\s\S]*?\bend\s+type\b/,
+    inside: {
+      'constant': /\b[A-Z_]+\b/,
+      'type-def': /\b(?:t|T)[A-Z][a-zA-Z]*\b/,
+      'comment': /\{.*?\}/,
+      'comment-line': /\/\/.*/,
+    },
   },
-  "const-name": {
-    pattern: /(?:[A-Z][A-Z_0-9]*)(?=\s*:)/,
-  },
-  "const-type": {
-    pattern: /(?:integer|real)/,
-  },
-  "comment-curly": {
-    pattern: /{.*}/,
-  }
+  'comment': /\{.*?\}/,
+  'comment-line': /\/\/.*/,
+  'string': /"(?:[^"\\]|\\.)*"/,
+  'number': /\b\d+\b/,
+  'definition': [
+    /\bconst\b|\bend\s+const\b/,
+    /\bvar\b|\bend\s+var\b/,
+    /\balgorithm\b|\bend\s+algorithm\b/,
+  ],
+  'keyword': /\b(?:for|while|do|end for|end while|if|then|else|end if)\b/,
+  'constant': /\b[A-Z_]+\b/,
+  'special-function':
+    /(readInteger|readReal|readChar|readString|readBoolean|writeInteger|writeReal|writeChar|writeString|writeBoolean|integerToReal|realToInteger|charToCode|codeToChar)/,
+  'type': /(integer|real|boolean|char|vector|string)/,
+  'type-def': /\b(?:t|T)[A-Z][a-zA-Z]*\b/,
+
+  'operator': /:=|<=|>=|<|>|=|≤|≠|≥|\+|-|\*|\/|\^|mod|and|or|not/,
+  'boolean': /\b(true|false)\b/,
 };
 
 function App() {
   const [code, setCode] = useState("");
 
-console.log(Prism.highlight(code, Prism.languages.your_language));
   return (
-    <Editor
-      value={code}
-      onValueChange={code => setCode(code)}
-      highlight={code => Prism.highlight(code, Prism.languages.your_language)}
-      padding={10}
-      style={{
-        fontFamily: '"Fira code", "Fira Mono", monospace',
-        fontSize: 12,
-      }}
-    />
+    <>
+      <h1>Insert algorithm</h1>
+      <Editor
+        value={code}
+        onValueChange={code => setCode(code)}
+        highlight={code => Prism.highlight(code, Prism.languages.your_language)}
+        padding={10}
+        style={{
+          fontFamily: '"Fira code", "Fira Mono", monospace',
+          fontSize: 12,
+        }}
+      />
+    </>
   );
 }
 
